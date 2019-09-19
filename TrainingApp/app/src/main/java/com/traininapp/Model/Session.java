1e2bba8c97a2d13c9ecb5c75d51645c6c6c42255
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-09-19 18:38:45 +0200

    Added instance variables and constructor in class Session

--- TrainingApp/app/src/main/java/com/traininapp/Model/Session.java
+++ TrainingApp/app/src/main/java/com/traininapp/Model/Session.java
@@ -1,4 +1,15 @@
 package com.traininapp.Model;
 
+import java.util.List;
+
 public class Session {
+    private String name;
+    private List<Exercise> exercises;
+    //date???
+    //time????
+
+    public Session(String name, List<Exercise> exercises) {
+        this.name = name;
+        this.exercises = exercises;
+    }
 }
