22295d09b0d96de6097533fe0397d4f65cd7e464
Isak Magnusson <gusmagis@student.gu.se>
2019-09-24 09:47:21 +0200

    Fixed so the app is working

---
+++ TrainingApp/app/src/main/java/com/traininapp/View/UpcomingFragment.java
@@ -0,0 +1,24 @@
+package com.traininapp.View;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.traininapp.R;
+
+public class UpcomingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_upcoming, null);
+    }
+}
