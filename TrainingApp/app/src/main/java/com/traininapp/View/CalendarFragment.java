5569806dd347b3c4c3001862936a6094553be7d2
adamtornkvist <adamtornqvist23@gmail.com>
2019-09-26 16:03:21 +0200

    All thats left to do is to connect the calender fragment with the view somehow.

--- TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
+++ TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
@@ -11,15 +11,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.traininapp.R;
 
 public class CalendarFragment extends Fragment {
 
+    private TextView myDate;
+    private CalendarView calendarView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //setCalendarView();
+        //setMyDate();
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                String date = (day) + "/" + month + "/" + year;
+                myDate.setText(date);
+            }
+        });
+
+
+
+
         return inflater.inflate(R.layout.fragment_calendar, null);
 
     }
+    /*
+    public void setCalendarView(){
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+    }
+
+    public void setMyDate(){
+        myDate = (TextView) findViewById(R.id.myDate);
+    }*/
+
+
+
+
+
+
+
+
 }
--- TrainingApp/app/src/main/java/com/traininapp/viewModel/CalendarViewModel.java
+++ TrainingApp/app/src/main/java/com/traininapp/viewModel/CalendarViewModel.java
@@ -3,9 +3,11 @@ package com.traininapp.viewModel;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import androidx.lifecycle.ViewModel;
+
 import com.traininapp.R;
 
-public class calendarViewModel {
+public class CalendarViewModel extends ViewModel {
 
     TextView myDate;
     CalendarView calendarView;
