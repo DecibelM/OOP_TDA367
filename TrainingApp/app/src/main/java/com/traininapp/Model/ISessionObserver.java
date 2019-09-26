4bdfba0b99254cda7b5c7f6e1e6e8fa04729b283
Viktor <fredholm97@gmail.com>
2019-09-26 14:33:50 +0200

    Added comments and finished up the code

--- TrainingApp/app/src/main/java/com/traininapp/Model/ISessionObserver.java
+++ TrainingApp/app/src/main/java/com/traininapp/Model/ISessionObserver.java
@@ -2,7 +2,14 @@ package com.traininapp.Model;
 
 import java.util.List;
 
+/**
+ * observes sessions and waits to pass an exercise list onward.
+ */
 public interface ISessionObserver {
-
+    /**
+     * passes an exercise list to whoever wants to aquire one when a session is done.
+     *
+     * @param exerciseList
+     */
     public void updateSessionStats(List<Exercise> exerciseList);
 }
--- TrainingApp/app/src/main/java/com/traininapp/Model/Statistic.java
+++ TrainingApp/app/src/main/java/com/traininapp/Model/Statistic.java
@@ -3,6 +3,10 @@ package com.traininapp.Model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This handles the bulk of the statistics controll and keeps an eye onall of it.
+ * It currently gets the data via an observer but in the future it should preferably collect it from a database.
+ */
 public class Statistic implements ISessionObserver {
 
     int totTrainingTime;
@@ -23,22 +27,24 @@ public class Statistic implements ISessionObserver {
 
     @Override
     public void updateSessionStats(List<Exercise> exerciseList) {
-        List<CardioExercise> cardioExercises = new ArrayList<>();
-        List<StrengthExercise> strengthExercises = new ArrayList<>();
 
-        for (Exercise exercise: exerciseList){                      //TODO separate cardio from strength exercises ant send to different methods accordingly
-           // updateCardioExercise();
+        for (Exercise exercise: exerciseList){
             if (exercise instanceof CardioExercise){            //instanceof checks if the exercise is of the class in question
-                cardioExercises.add((CardioExercise) exercise);
+                updateCardioExercise((CardioExercise) exercise);
             } else if (exercise instanceof StrengthExercise){
-                strengthExercises.add((StrengthExercise) exercise);
+                updateStrengthExercise((StrengthExercise) exercise);
             } else {
-                //TODO throw the right type of exception for non implemented exercise type
-                System.out.println("there is no file of this type");
+
+                throw new IllegalArgumentException("The exercise must be of the type; 'CardioExercise' or 'StrengthExerxise'");
             }
         }
     }
 
+    /**
+     * Takes in an exercise of the type 'Strength' to update the relevant information in the Statistics class.
+     * Preferably in the future it should also split the information based on exercise type and divide it accordingly.
+     * @param exercise
+     */
     private void updateStrengthExercise(StrengthExercise exercise) {    //TODO this should also update the statistics of a specific exercise
 
         int tempWeight = exercise.getWeight();
@@ -53,10 +59,64 @@ public class Statistic implements ISessionObserver {
         totWeightLifted = totWeightLifted + exercise.getWeight()*exercise.getReps()*exercise.getSets();
     }
 
+    /**
+     * Takes in an exercise of the type 'Cardio' to update the relevant information in the Statistics class.
+     * Preferably in the future it should also split the information based on exercise type and divide it accordingly.
+     * @param exercise
+     */
     private void updateCardioExercise(CardioExercise exercise){         //TODO this should also update the statistics of a specific exercise
 
         totTrainingTime = totTrainingTime + exercise.getRunningTime();
 
         totDistance = totDistance + exercise.getDistance();
+    }
+
+    /**
+     *
+     * @return the total traing time
+     */
+    public int getTotTrainingTime() {
+        return totTrainingTime;
+    }
+
+    /**
+     *
+     * @return Total running distance
+     */
+    public int getTotDistance() {
+        return totDistance;
+    }
+
+    /**
+     *
+     * @return total ammount of weight reps
+     */
+    public int getTotReps() {
+        return totReps;
+    }
+
+    /**
+     *
+     * @return total ammount of weight sets
+     */
+    public int getTotSets() {
+        return totSets;
+    }
+
+    /**
+     * This is the total ammount of weight lifted, this is calculated by [weight]*[sets]*[reps]
+     *
+     * @return total weight lifted (int)
+     */
+    public int getTotWeightLifted() {
+        return totWeightLifted;
+    }
+
+    /**
+     *
+     * @return Highest value of weight lifted
+     */
+    public int getMaxWeight() {
+        return maxWeight;
     }
 }
