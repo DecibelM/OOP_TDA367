f5d276b888e217033cf7c577fda0ba35a223ee43
adamtornkvist <adamtornqvist23@gmail.com>
2019-09-26 14:34:39 +0200

    There is a calendar in the app. No functionality though, only visual stuff.
    Created a Viewmodel for the calendar. Not sure how to get it to work.

---
+++ TrainingApp/app/src/main/java/com/traininapp/viewModel/calendarViewModel.java
@@ -0,0 +1,32 @@
+package com.traininapp.viewModel;
+
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.traininapp.R;
+
+public class calendarViewModel {
+
+    TextView myDate;
+    CalendarView calendarView;
+
+
+    /*public void setCalendarView(){
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+    }
+
+    public void setMyDate(){
+        myDate = (TextView) findViewById(R.id.myDate);
+    }
+
+
+    //setContentView(R.layout.fragment_calendar);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                String date = (day) + "/" + month + "/" + year;
+                myDate.setText(date);
+            }
+        }); */
+}
