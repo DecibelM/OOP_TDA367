7249273e605bc5de1a9a505179156261d1ba09a0
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-10-03 20:09:45 +0200

    Created list that will be able to show upcoming sessions in the calendar.

--- TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
+++ TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
@@ -7,14 +7,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ListAdapter;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.traininapp.R;
+
+import java.util.ArrayList;
 
 
 /**
@@ -25,6 +31,8 @@ public class CalendarFragment extends Fragment {
 
     private TextView myDate;
     private CalendarView calendarView;
+    private ListView listView;
+    private String[] list;
 
     @Nullable
     @Override
@@ -32,17 +40,27 @@ public class CalendarFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calendar, null);
         myDate = (TextView) view.findViewById(R.id.myDate);
         calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        listView = (ListView) view.findViewById(R.id.listViewCalendar);
+
+        final Context context = this.getContext();
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 String date = (day) + "/" + month + "/" + year;
                 myDate.setText(date);
+                list[0] = "Löpning";
+                list[1] = "Yoga";
+                ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+
             }
         });
 
+        list = new String[]{"hej", "hjhej"} ;
 
-
+        listView = (ListView) view.findViewById(R.id.listViewCalendar);
+        ArrayAdapter adapter = new ArrayAdapter(this.getContext(),android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
 
         return view;
 
