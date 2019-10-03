61e9858940c63563527791ea0b0d3cdd3512d861
Viktor <fredholm97@gmail.com>
2019-10-03 10:08:59 +0200

    Set up for the recyclerview to work

--- TrainingApp/app/src/main/java/com/traininapp/View/GoalsFragment.java
+++ TrainingApp/app/src/main/java/com/traininapp/View/GoalsFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +16,42 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.traininapp.R;
+import com.traininapp.adapter.StatisticsAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Keeps hold of the goals page or my page. Also holds the recyclerview along with its adapter and Layout manager.
+ */
 public class GoalsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> listExample;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_goals, null);
+        View view = inflater.inflate(R.layout.fragment_goals, null);
+        listExample.add("hello World!");
+        listExample.add("This is a test!");
+        listExample.add("to see if all the texts will appear.");
+        listExample.add("And as you may see:");
+        listExample.add("It works!!! :)");
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.myPagesRecyclerViewID);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerViewAdapter = new StatisticsAdapter();
+
+        layoutManager= new LinearLayoutManager(GoalsFragment.super.getContext());
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        return view;
     }
 }
 
