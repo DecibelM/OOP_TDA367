0f36cb1f615fda6f29c576c5f735bcec6b3a800a
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-10-11 16:03:04 +0200

    Fixed some errors with tests and naming.

--- TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
+++ TrainingApp/app/src/main/java/com/traininapp/View/CalendarFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 
 import android.view.LayoutInflater;
