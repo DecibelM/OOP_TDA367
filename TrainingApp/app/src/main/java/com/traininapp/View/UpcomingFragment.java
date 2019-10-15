6519c9e9b30f58bca9d0e288b821e182face8984
adamtornkvist <adamtornqvist23@gmail.com>
2019-10-15 18:41:49 +0200

    More work on the selectedSession page. You can now get to the view from the calendar and you can also leave it with the done button. Created som new classes to handle the new view.

--- TrainingApp/app/src/main/java/com/traininapp/View/UpcomingFragment.java
+++ TrainingApp/app/src/main/java/com/traininapp/View/UpcomingFragment.java
@@ -18,6 +18,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.traininapp.R;
 import com.traininapp.viewModel.AddSession;
 import com.traininapp.viewModel.CreateSession;
+import com.traininapp.viewModel.SelectedSession;
 import com.traininapp.viewModel.UpcomingSessionsViewModel;
 
 public class UpcomingFragment extends Fragment {
@@ -74,6 +75,12 @@ public class UpcomingFragment extends Fragment {
         Intent intent = new Intent(getActivity(), AddSession.class);
 
         // Starting the activity
+        startActivity(intent);
+    }
+
+    public void openSession(){
+        Intent intent = new Intent(getActivity(), SelectedSession.class);
+
         startActivity(intent);
     }
 
