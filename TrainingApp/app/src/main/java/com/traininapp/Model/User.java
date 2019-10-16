a92540ecb1705e9737013867f93a67c603e7a08d
Viktor <fredholm97@gmail.com>
2019-10-16 16:28:53 +0200

    fixed routine bug

--- TrainingApp/app/src/main/java/com/traininapp/Model/User.java
+++ TrainingApp/app/src/main/java/com/traininapp/Model/User.java
@@ -1,10 +1,14 @@
 package com.traininapp.Model;
 
+import com.traininapp.Model.Planning.*;
+import com.traininapp.Model.Statistics.Results;
 import com.traininapp.Model.Planning.Exercise;
 import com.traininapp.Model.Planning.Planner;
 import com.traininapp.Model.Planning.Routine;
 import com.traininapp.Model.Statistics.Goal;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +21,8 @@ public class User {
 
     private List<Goal> goalList;
     private Planner planner;
-    private List<Routine> routineList = new ArrayList<>();
+    private List<Routine> routineList;
+    private Results results;
     private Routine routine;
 
     /**
@@ -27,6 +32,8 @@ public class User {
     public User( Planner planner) {
         this.goalList = new ArrayList<>();
         this.planner = planner;
+        this.results = new Results();
+        routineList = new ArrayList<>();
     }
 
     public void addRoutine(String name, List<Exercise> exerciseList){
