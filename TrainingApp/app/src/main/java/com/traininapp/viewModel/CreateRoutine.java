9d49dbf5f3d4b4407a408b248aaf6b6169352a25
Mathias <lammers@student.chalmers.se>
2019-10-20 22:44:59 +0200

    Added javaDoc and comments to code

--- TrainingApp/app/src/main/java/com/traininapp/viewModel/CreateRoutine.java
+++ TrainingApp/app/src/main/java/com/traininapp/viewModel/CreateRoutine.java
@@ -25,201 +25,210 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class CreateRoutine extends AppCompatActivity implements Serializable {
 
-        private EditText txtEnterSessionName;
+    // Declaring elements
+    private EditText txtEnterSessionName;
+    private Button btnAddNewExercise, btnDone;
 
-        //List for all created fragments
-        private List<FragStrRow> listStrFrag = new ArrayList<>();
-       private List<FragCarRow> listCarFrag = new ArrayList<>();
+    // Lists for created fragments
+    private List<FragStrRow> listStrFrag = new ArrayList<>();
+    private List<FragCarRow> listCarFrag = new ArrayList<>();
 
-       private Repository repository;
+    // List for exercises
+    private List<Exercise> exerciseList = new ArrayList<>();
 
-
-
-        private boolean control;
-
-        //private DatabaseHelper myDb = new DatabaseHelper(this);
-
-
-        //List for all exercises
-        private List<Exercise> exerciseList = new ArrayList<>();
-
-        private FragmentTransaction fragmentTransaction;
-
-        //boolean to see which type of exercise is currently selected
-        private boolean isStrength = true;
-
-/*        private StrExTable strExTable;
-        private CarExTable carExTable;*/
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_create_routine);
-            repository = Repository.getInstance();
+    // Repo
+    private Repository repository;
 
 
-            ToggleButton togCardioOrStrength = findViewById(R.id.togCardioOrStrengthID);
-            Button btnAddnewExersice = findViewById(R.id.btnAddExerciseID);
-            Button btnDone = findViewById(R.id.btnDoneID);
+    private FragmentTransaction fragmentTransaction;
+    private boolean control;
 
-            final LinearLayout rowStrExerciseInfoID = findViewById(R.id.rowStrExerciseInfoID);
-            final LinearLayout rowCarExerciseInfoID = findViewById(R.id.rowCarExerciseInfoID);
-            txtEnterSessionName = findViewById(R.id.txtEnterSessionNameID);
+    // Database stuff, currently not in use
+/*    private DatabaseHelper myDb = new DatabaseHelper(this);
+    private StrExTable strExTable;
+    private CarExTable carExTable;*/
 
-            //hide the titles for cardio exercises when activity starts
-            rowCarExerciseInfoID.setVisibility(View.GONE);
+    //boolean to see which type of exercise is currently selected
+    private boolean isStrength = true;
 
-            //depending on if toggle is enabled, hide or show relevant titles
-            //also change boolean isStrength, for creating the right exercise
-            togCardioOrStrength.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    //for cardio
-                    if (isChecked) {
-                        selectType(rowCarExerciseInfoID, rowStrExerciseInfoID);
-                        isStrength = false;
-                    }
-                    //for strength
-                    else {
-                        // The toggle is disabled
-                        selectType(rowStrExerciseInfoID, rowCarExerciseInfoID);
-                        isStrength = true;
-                    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_routine);
+
+        // Initializing the repository
+        repository = Repository.getInstance();
+
+        // Initializing elements
+        ToggleButton togCardioOrStrength = findViewById(R.id.togCardioOrStrengthID);
+        btnAddNewExercise = findViewById(R.id.btnAddExerciseID);
+        btnDone = findViewById(R.id.btnDoneID);
+        txtEnterSessionName = findViewById(R.id.txtEnterSessionNameID);
+        final LinearLayout rowStrExerciseInfoID = findViewById(R.id.rowStrExerciseInfoID);
+        final LinearLayout rowCarExerciseInfoID = findViewById(R.id.rowCarExerciseInfoID);
+
+        //hide the titles for cardio exercises when activity starts
+        rowCarExerciseInfoID.setVisibility(View.GONE);
+
+        //depending on if toggle is enabled, hide or show relevant titles
+        //also change boolean isStrength, for creating the right exercise
+        togCardioOrStrength.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //for cardio
+                if (isChecked) {
+                    selectType(rowCarExerciseInfoID, rowStrExerciseInfoID);
+                    isStrength = false;
                 }
-            });
-
-            btnAddnewExersice.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //if strength is chosen, create strength row
-                    if (isStrength) {
-                        createFragRow();
-                    } else {
-                        //if strength is not chosen, create cardio row
-                        createFragRow();
-                    }
+                //for strength
+                else {
+                    // The toggle is disabled
+                    selectType(rowStrExerciseInfoID, rowCarExerciseInfoID);
+                    isStrength = true;
                 }
-            });
+            }
+        });
 
-            // Clicking the Done button, directing the user to the Upcoming session view
-            btnDone.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onDoneClick();
+        btnAddNewExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if strength is chosen, create strength row
+                if (isStrength) {
+                    createFragRow();
+                } else {
+                    //if strength is not chosen, create cardio row
+                    createFragRow();
                 }
-            });
+            }
+        });
+
+        // Clicking the Done button, saving Session and directing the user to the Upcoming session view
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDoneClick();
+            }
+        });
     }
 
-        //open new activity
-        public void onDoneClick() {
+    /**
+     * Method called when user press Done button. Saves the information as a new Session and
+     * directs the user to the Upcoming Session view
+     */
+    public void onDoneClick() {
 
-            // Get the Session name from user input
-            String sessionName = txtEnterSessionName.getText().toString();
+        // Get the Session name from user input
+        String sessionName = txtEnterSessionName.getText().toString();
 
-            // Remove all previously added exercises
-            exerciseList.clear();
+        // Remove all previously added exercises
+        exerciseList.clear();
 
-            //Set boolean control true, used for seeing if
-            //everything goes okay when saving the routine
-            control = true;
+        // Set boolean control true, used for seeing if
+        // everything goes okay when saving the routine
+        control = true;
 
-            // Using the fragments created to save exercises in exerciseList
-            fragStrToStrExList();
-            fragCarToCarExList();
+        // Using the fragments created to save exercises in exerciseList
+        fragStrToStrExList();
+        fragCarToCarExList();
 
-            // Controlling if valid user input
-            checkName(sessionName);
-            checkNameLength(sessionName);
+        // Controlling if valid user input
+        //checkName(sessionName);
+        checkNameLength(sessionName);
 
-            // If no fragments returned null and name is unique
-            if (control == true) {
+        // If no fragments returned null and name is unique
+        if (control == true) {
 
-                // Adding Session to users list
-                repository.getUser().getPlanner().addSession(sessionName, LocalDate.now(), exerciseList);
+            // Adding Session to users list
+            repository.getUser().getPlanner().addSession(sessionName, LocalDate.now(), exerciseList);
 
-                // Give feedback that the routine has been saved
-                String toastMessage = "Session: " + sessionName + " has been saved!";
-                Toast.makeText(CreateRoutine.this, toastMessage, Toast.LENGTH_SHORT).show();
+            // Give feedback that the routine has been saved
+            String toastMessage = "Session: " + sessionName + " has been saved!";
+            Toast.makeText(CreateRoutine.this, toastMessage, Toast.LENGTH_SHORT).show();
 
                         /*RoutineTable routineTable = new RoutineTable(getApplicationContext());
                         routineTable.insertRoutineData(routineName);
                         insertStrExInDB(routineName);
                         insertCarExInDB(routineName);*/
 
-                // Clear Session name field
-                txtEnterSessionName.setText("");
-            }
-
-            // Directing the user to Upcoming session view
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            // Clear Session name field
+            txtEnterSessionName.setText("");
         }
 
-        // Create a new strength fragment in the row
-        public void createFragRow() {
+        // Directing the user to Upcoming session view
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
-         // Create the fragment
-         Fragment fragment;
+    /**
+     * Method which adds another row fragment, allowing the user to add exercises
+     */
+    public void createFragRow() {
 
-         //Begin the transaction, to start doing something with the fragment
-         fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        // Create the fragment
+        Fragment fragment;
 
-         if(isStrength){
-             fragment = new FragStrRow();
-             listStrFrag.add((FragStrRow) fragment);
+        //Begin the transaction, to start doing something with the fragment
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-         } else{
-             fragment = new FragCarRow();
-             listCarFrag.add((FragCarRow) fragment);
-         }
+        if (isStrength) {
+            fragment = new FragStrRow();
+            listStrFrag.add((FragStrRow) fragment);
 
-         // Add the created fragment to "displayRowsID"
-         fragmentTransaction.add(R.id.displayRowsID, fragment);
-
-         // Commit and finish the FragmentTransaction
-         fragmentTransaction.commit();
+        } else {
+            fragment = new FragCarRow();
+            listCarFrag.add((FragCarRow) fragment);
         }
 
-        // hide or show which type of exercise is selected, depending on toggle
-        public void selectType(LinearLayout visRow, LinearLayout gonRow) {
-            visRow.setVisibility(View.VISIBLE);
-            gonRow.setVisibility(View.GONE);
-        }
+        // Add the created fragment to "displayRowsID"
+        fragmentTransaction.add(R.id.displayRowsID, fragment);
 
-        //if the fragment had been removed remove it from the list of added exercises
-        public void removeDeletedExercises() {
-            if (exerciseList.get(exerciseList.size() - 1).getName() == "REMOVE ME") {
-                exerciseList.remove(exerciseList.size() - 1);
-            }
-        }
+        // Commit and finish the FragmentTransaction
+        fragmentTransaction.commit();
+    }
 
-        //if failing to save exercise
-        public void failSave() {
-            exerciseList.clear();
-            control = false;
-        }
+    // hide or show which type of exercise is selected, depending on toggle
+    public void selectType(LinearLayout visRow, LinearLayout gonRow) {
+        visRow.setVisibility(View.VISIBLE);
+        gonRow.setVisibility(View.GONE);
+    }
 
-         //Checks the name of all added routines to see if entered name is unique
-         public void checkName(String name){
-            for (int i = 0; i < repository.getUser().getRoutineList().size(); i++){
-                if (repository.getUser().getRoutineList().get(i).getName().equals(name)){
-                    String toastMessage = "Name: " + name + " is not unique";
-                    Toast.makeText(CreateRoutine.this, toastMessage, Toast.LENGTH_SHORT).show();
-                    control = false;
-                }
-            }
+    //if the fragment had been removed remove it from the list of added exercises
+    public void removeDeletedExercises() {
+        if (exerciseList.get(exerciseList.size() - 1).getName() == "REMOVE ME") {
+            exerciseList.remove(exerciseList.size() - 1);
         }
+    }
 
-        public void checkNameLength(String name){
-            if(name.length() == 0){
-                String toastMessage = "No name entered!";
+    //if failing to save exercise
+    public void failSave() {
+        exerciseList.clear();
+        control = false;
+    }
+
+/*    //Checks the name of all added routines to see if entered name is unique
+    public void checkName(String name) {
+        for (int i = 0; i < repository.getUser().getRoutineList().size(); i++) {
+            if (repository.getUser().getRoutineList().get(i).getName().equals(name)) {
+                String toastMessage = "Name: " + name + " is not unique";
                 Toast.makeText(CreateRoutine.this, toastMessage, Toast.LENGTH_SHORT).show();
                 control = false;
             }
         }
+    }*/
+
+    /**
+     * Method to check if string is at least one character long. If not, setting control to false
+     * @param name String input from user
+     */
+    public void checkNameLength(String name) {
+        if (name.length() == 0) {
+            String toastMessage = "No name entered!";
+            Toast.makeText(CreateRoutine.this, toastMessage, Toast.LENGTH_SHORT).show();
+            control = false;
+        }
+    }
 
 /*        public void insertStrExInDB(String name){
             for(Exercise exercise : exerciseList) {
@@ -247,43 +256,55 @@ public class CreateRoutine extends AppCompatActivity implements Serializable {
 
          }*/
 
-         public void fragStrToStrExList(){
-             //go through the list of all created strength fragments
-             for (FragStrRow fr : listStrFrag) {
+    /**
+     * Method used for creating strength Exercises based on data in strength row fragments
+     */
+    public void fragStrToStrExList() {
 
-                 //if saveInfo (in FragStrRow) did not return null, add to list of exercises
-                 if (fr.saveInfo() != null) {
-                     exerciseList.add(fr.saveInfo());
+        // Go through the list of all created strength fragments
+        for (FragStrRow fr : listStrFrag) {
 
-                     //remove exercises of deleted fragments
-                     removeDeletedExercises();
+            // If saveInfo (in FragStrRow) did not return null, add to list of exercises
+            if (fr.saveInfo() != null) {
+                exerciseList.add(fr.saveInfo());
 
-                     //if it had returned null
-                 } else {
-                     //run failsave and break from loop
-                     failSave();
-                     break;
-                 }
-             }
-         }
+                // Remove exercises of deleted fragments
+                removeDeletedExercises();
 
-       public void fragCarToCarExList() {
-          //go through the list of all created cardio fragments
-          for (FragCarRow fr : listCarFrag) {
+            // If it had returned null
+            } else {
 
-                //if saveInfo (in FragCarRow) did not return null, add to list of exercises
-                //and if control is still true (no strengthexercises returned null)
-                if (fr.saveInfo() != null && control == true) {
-                   exerciseList.add(fr.saveInfo());
+                //run failsave and break from loop
+                failSave();
 
-                   //remove exercises of deleted fragments
-                  removeDeletedExercises();
+                break;
+            }
+        }
+    }
 
-                 //if it had returned null
-              } else {
-                 //run failsave and break from loop
-                 failSave();
-                 break;
+    /**
+     * Method used for creating cardio Exercises based on data in cardio row fragments
+     */
+    public void fragCarToCarExList() {
+
+        // Go through the list of all created cardio fragments
+        for (FragCarRow fr : listCarFrag) {
+
+            // If saveInfo (in FragCarRow) did not return null, add to list of exercises
+            // and if control is still true (no strengthexercises returned null)
+            if (fr.saveInfo() != null && control == true) {
+                exerciseList.add(fr.saveInfo());
+
+                // Remove exercises of deleted fragments
+                removeDeletedExercises();
+
+            // If it had returned null
+            } else {
+
+                // Run failsave and break from loop
+                failSave();
+
+                break;
             }
         }
     }
