c0d594c46808c9f4c6aaa6453ec19764bc62161c
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-10-23 15:23:02 +0200

    Merging with master

--- TrainingApp/app/src/main/java/com/traininapp/MainActivity.java
+++ TrainingApp/app/src/main/java/com/traininapp/MainActivity.java
@@ -3,9 +3,12 @@ package com.traininapp;
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.traininapp.Model.*;
+import com.traininapp.Model.Planning.CardioExercise;
 import com.traininapp.Model.Planning.Exercise;
 import com.traininapp.Model.Planning.Planner;
 import com.traininapp.Model.Planning.Session;
+import com.traininapp.Model.Planning.StrengthExercise;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,6 +16,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,28 +49,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeDummySessions(){
         List<Exercise> eList = new ArrayList<>();
+        eList.add(new CardioExercise("Spring",10,10));
+        eList.add(new StrengthExercise("Lyft",10,100,10));
+        eList.add(new CardioExercise("Spring2",100,10));
+        eList.add(new StrengthExercise("Lyft2",10,10,10));
+
         repo.addSession("Löpning", eList, LocalDate.now(),R.drawable.workout_5);
         repo.addSession("Yoga", eList, LocalDate.now().plusDays(1), R.drawable.workout_2);
         repo.addSession("Armträning", eList, LocalDate.now().plusDays(2),R.drawable.workout_4);
         repo.addSession("Hjärngympa", eList,LocalDate.now().plusDays(3),R.drawable.workout_1);
-    private void initializeDummySessions(Planner planner){
-
-        planner.addSession("Löpning", LocalDate.now(),R.drawable.workout_5);
-        planner.addSession("Yoga", LocalDate.now().plusDays(1),R.drawable.workout_2);
-        planner.addSession("Yoga2", LocalDate.now().plusDays(1),R.drawable.workout_2);
-        planner.addSession("Yoga3", LocalDate.now().plusDays(1),R.drawable.workout_2);
-        planner.addSession("Armträning", LocalDate.now().plusDays(2),R.drawable.workout_4);
-        planner.addSession("Hjärngympa", LocalDate.now().plusDays(3),R.drawable.workout_1);
-
-
-
-        Session session = planner.getSessionList().get(0);
-        session.addCardioExercise("Spring",10,10);
-        session.addStrengthExercise("Lyft",10,100,10);
-        session.addCardioExercise("Spring2",100,10);
-        session.addStrengthExercise("Lyft2",10,10,10);
-        session.addCardioExercise("Spring3",100,100);
-        session.addStrengthExercise("Lyft3",100,100,100);
 
     }
 }
